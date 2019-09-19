b8a7cafb6da8de7194239de1d4843c03442c202c
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-09-18 21:49:20 -0400

    added map component

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -2,93 +2,42 @@ import React from 'react';
 import { withGoogleMap, withScriptjs, GoogleMap, Polyline, Marker } from 'react-google-maps';
 import { Col, Row, Container } from "../components/Grid";
 import Notification from "../components/Notification/Notification";
+import Nav from "../components/Nav/"
+import Main from "../components/Main"
+import Map from "../components/Map"
 
 
-class Map extends React.Component {
+class Home extends React.Component {
   state = {
     progress: [],
-    loading: true
-  }
-
-  initialLocation = () => { 
-    const getPosition = function (options) {
-      return new Promise(function (resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-      });
-    }
-    
-    getPosition()
-      .then((position) => {
-        const { latitude, longitude } = position.coords
-
-        this.setState({ 
-          progress: [{lat: latitude, lng: longitude}],
-          loading: false
-        })
-        this.watchPosition()
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
-  }
-
-  watchPosition = () => {
-    navigator.geolocation.watchPosition(
-      (position) => {
-        let location = this.state.progress.concat({lat:position.coords.latitude,lng:position.coords.longitude})
-        this.setState({ progress: location })
-      }
-    )
-  }
-  
-  componentDidMount = () => { 
-    this.initialLocation()
+    loading: true,
+    page:"Home"
   }
   
   render() {
-    const { loading, progress } = this.state;
-    console.log(progress)
-    // Check if we have a position, if not, do not load map
-    if (loading) {
-      return null;
-    }
+    const MapComponent = withScriptjs(withGoogleMap(Map))
       return (
-        <GoogleMap
-          defaultZoom={16}
-          defaultCenter={{lat:progress[0].lat, lng:progress[0].lng}}
-          >
-            { this.state.progress && (
-              <>
-                {/* Set path */}
-                <Polyline path={progress} options={{ strokeColor: "#FF0000 "}} />
-                {/* Set marker to last known location */}
-                <Marker position={progress[progress.length - 1]} />
-              </>
-            )}
-        </GoogleMap>
+        <div>
+          {console.log(this.state)}
+          <Nav
+            page={this.state.page}
+          />
+          <Row>
+            <Col size="md-10 xs-12">
+              <MapComponent
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100%`, width: '100%' }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            </Col>
+            <Col size="md-2 xs-12">
+              <Notification />
+            </Col>
+          </Row>
+        </div>
       )
-    }
   }
+}
 
-
-const MapComponent = withScriptjs(withGoogleMap(Map))
-
-export default () => (
-  <Container fluid>
-    <Row> 
-      <Col size="md-10 xs-12">
-        <MapComponent
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%`, width: '100%' }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </Col>
-     
-      
-      <Col size="md-2 xs-12">
-        <Notification />
-      </Col>
-    </Row>
-  </Container>
-)
+export default Home
