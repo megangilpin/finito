11947ada71e5fb7d0c6add21e38bf058542b25c8
Percy A <percy.addoyobo@gmail.com>
2019-09-26 19:00:34 -0400

    buttons for sidebar

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Sidebar.css"
+import "./Sidebar.css";
+import {Link} from "react-router-dom"
 
 const sideBar = props => {
   let drawerClasses = "side-bar";
@@ -12,11 +13,23 @@ const sideBar = props => {
         <img src="/images/logo.png" width="80px" alt="Arryvl Logo" />
       </center>
     </div>
-    <ul>
+    <div>
+      <Hidden smDown></Hidden>
+    </div>
+    <MenuList>
+      <MenuItem component = {Link} to="/"> Start Trip </MenuItem>
+    </MenuList>
+    <MenuList>
+      <MenuItem component = {Link} to="/Friends"> Friends </MenuItem>
+    </MenuList>
+    <MenuList>
+      <MenuItem component = {Link} to="/Destinations"> Destinations </MenuItem>
+    </MenuList>
+    {/* <ul>
       <li><p href="/">Start Trip</p></li>
       <li><p href="/">Friends</p></li>
       <li><p href="/">Destinations</p></li>
-    </ul>
+    </ul> */}
   </nav>
   );
 };
