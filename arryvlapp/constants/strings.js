dea2821eb40bff5eaccdccb143f9ac17b05e777d
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-09-27 23:39:07 -0400

    try again

--- arryvlapp/constants/strings.js
+++
@@ -1,8 +0,0 @@
-const strings = {
-  LOGIN: "Log In",
-  WELCOME_TO_LOGIN: "Welcome to the login screen!",
-  EMAIL_PLACEHOLDER: "Email",
-  PASSWORD_PLACEHOLDER: "Password"
-};
-
-export default strings;
