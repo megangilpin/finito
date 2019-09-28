3ca1d5086eb160d73178380d48ef13eb6438c566
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-09-27 21:40:03 -0400

    fixed nav

--- client/src/components/FriendNav/index.js
+++ client/src/components/FriendNav/index.js
@@ -5,7 +5,7 @@ import "./Nav.css";
 
 function Nav(props) {
   return (
-    <nav className="navbar navbar-light navbar-expand-md bg-light sticky-top">
+    <nav className="navbar navbar-light navbar-expand-md bg-light">
       <span className="navbar-brand d-flex flex-fill"><a href="/"><img src="/images/logo.png" width="30px" alt="Arryvl Logo" /></a></span>
       <div>
         <label><strong>Track your Friends Trip</strong></label>
