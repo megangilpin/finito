7a0885c97ae7a310c32bd81c3046c8489eb620ec
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-09-27 21:05:33 -0400

    added controller

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,20 +7,24 @@ export default {
     return await axios.post("/googlemap/geocode", address);
   },
   distanceMatrix: async (distanceMatrixInfo) => {
-    return await axios.post("/googlemap/distanceMatrix", distanceMatrixInfo )
+    return await axios.post("/googlemap/distanceMatrix", distanceMatrixInfo)
   },
   // Saves a trip to the database
   updateTrip: async (tripId, progress, userId, tripTime, endTrip) => {
-    return await axios.post("/googlemap/updateTrip", {tripId, progress, userId, tripTime, endTrip});
-  }, 
+    return await axios.post("/googlemap/updateTrip", { tripId, progress, userId, tripTime, endTrip });
+  },
   // Saves a trip to the database
   arrivalText: async (phone) => {
-    return await axios.post("/notification/text/arrival", {phone});
+    return await axios.post("/notification/text/arrival", { phone });
   },
   startTripText: async (phone, tripURL) => {
-    return await axios.post("/notification/text", {phone, tripURL});
+    return await axios.post("/notification/text", { phone, tripURL });
   },
   getTrip: async (trip_id) => {
     return await axios.get("/googlemap/getTrip/" + trip_id);
+  },
+  getUser: async (userId) => {
+    console.log("userid: " + userId)
+    return await axios.post("/googlemap/getUser", { userId });
   }
 };
